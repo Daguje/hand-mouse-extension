27d10d26043d8cc2e11616005863956b935c82e3
Daniel Gustavo Favero <danielfavero17@gmail.com>
2023-04-22 08:20:08 -0300

    fix: ajustado comunicação dos contentscripts com serviceworker

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,4 +1,58 @@
-import { Camera } from './lib'
+class Camera {
+    video: HTMLVideoElement
+  
+    constructor() {
+        this.video = document.createElement("video") as HTMLVideoElement
+    }
+    
+    private static getVideoConfig() {
+        return {
+            audio: false,
+            video: {
+                width: globalThis.screen.availWidth,
+                height: globalThis.screen.availHeight,
+                frameRate: {
+                    ideal: 60
+                }
+            }
+        }
+    }
+  
+    static drawCamera(camera: Camera) {
+        camera.video.height = 240
+        camera.video.width = 320
+        camera.video.style.transform = 'scaleX(-1)'
+        camera.video.style.position = 'fixed'
+        camera.video.style.top = '16px'
+        camera.video.style.left = '16px'  
+        camera.video.style.zIndex = '9999'  
+  
+        document.body.append(camera.video)
+    }
+  
+    static async init() {
+        if(!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia){
+            throw new Error('API do Browser navigator.mediaDevices.getUserMedia não está disponível')
+        }
+  
+        const stream = await navigator.mediaDevices.getUserMedia(this.getVideoConfig())
+  
+        const camera = new Camera()
+        camera.video.srcObject = stream
+        this.drawCamera(camera)
+        
+        // Permissão da Câmera
+        await new Promise(resolve => {
+            camera.video.onloadedmetadata = () => {
+                resolve(camera.video)
+            }
+        })
+  
+        camera.video.play()
+  
+        return camera
+    }
+}
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (!message) return
@@ -7,8 +61,13 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         case 'UPDATED':
             (async () => {
                 const camera = await Camera.init()
-                console.log({camera})
-                sendResponse({ camera })
+
+                const canvas = new OffscreenCanvas(camera.video.width, camera.video.height);
+                const ctx = canvas.getContext('2d');
+                ctx.drawImage(camera.video, 0, 0);
+                const video = ctx.getImageData(0, 0, camera.video.width, camera.video.height);
+
+                sendResponse({ video: video.data })
             })();
             return true
         case 'HANDS_PREDICTED':
--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -1,21 +1,35 @@
-import { Controller } from './lib'
-
 import * as handPoseDetection from "@tensorflow-models/hand-pose-detection"
 import * as mediapipeHands from "@mediapipe/hands"
 
-const controller = new Controller({ mediapipeHands, handPoseDetection })
+class Controller {
+    async estimateHands(video: any) {
+        const model = handPoseDetection.SupportedModels.MediaPipeHands;
+
+        const detector = await handPoseDetection.createDetector(model, {
+            runtime: 'mediapipe',
+            modelType: 'full',
+            maxHands: 2,
+            solutionPath: `https://cdn.jsdelivr.net/npm/@mediapipe/hands@${mediapipeHands.VERSION}`
+        });
+
+        const hands = await detector.estimateHands(video, { flipHorizontal: true })
+
+        console.log({ hands })
+    }
+}
+
+const controller = new Controller()
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     (async () => {
         chrome.tabs.sendMessage(tabId, { action: 'UPDATED' }, async (response) => {
-            if (!response.camera) {
+            if (!response.video) {
                 console.error('Não foi possível obter informações da camera');
-                return;
             }
 
-            console.log(response)
+            console.log(response.video)
         
-            await controller.estimateHands(response.camera)
+            await controller.estimateHands(response.video)
         })
     })();
 })
