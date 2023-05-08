d5883aa704747dd0e0e002c7e4eb26a4decf6ffd
Jeferson Souza <souzajefison@gmail.com>
2023-05-08 20:01:20 -0300

    chore: primeira versão sem tailwind - 🔧

--- src/popup.ts
+++ src/popup.ts
@@ -3,3 +3,9 @@ import '../styles/popup.scss';
 document.getElementById('go-to-options').addEventListener('click', () => {
   chrome.runtime.openOptionsPage();
 });
+document.getElementById('go-to-options').addEventListener('click', () => {
+  chrome.runtime.openOptionsPage();
+});
+document.getElementById('go-to-options').addEventListener('click', () => {
+  chrome.runtime.openOptionsPage();
+});
